8930841d7a2b7d3ffb49f7f00925e8294231e1ab
LostProgrammer42 <jstavan26@yahoo.com>
2025-10-28 22:34:23 +0530

    Initial Commit

---
+++ Corner_Stitch.cpp
@@ -0,0 +1,9 @@
+// Author: Stavan Mehta
+// Version: Pilot Initial (V0)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MIN_VALUE (numeric_limits<long>::min())
+#define MAX_VALUE (numeric_limits<long>::max())
+
